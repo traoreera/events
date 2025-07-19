e82a55d38c18a5e43ba35f840c85034c604611b7
thedashing <traoreera@gmail.com>
2025-07-19 12:32:18 +0000

    dernier commit gestion des events

---
+++ run.py
@@ -0,0 +1,27 @@
+import deps
+from fasthtml.common import Request
+
+PLUGIN_INFO = {
+    "name": "Events",
+    "version": "1.0.0",
+    "author": "traore Eliezer",
+    "Api_prefix": "/app/events",
+    "tag_for_identified": ["Plugin", "events"],
+    "trigger": 2,
+}
+
+router = deps.APIRouter(
+    prefix=PLUGIN_INFO["Api_prefix"],
+)
+
+
+class Plugin:
+
+    def __init__(self):
+        super().__init__()
+        return
+
+    @router("/", methods=["GET"])
+    def run(session, request: Request):
+        session["user_id"] = "bc523c62-6d38-49f1-9741-124886"
+        return
